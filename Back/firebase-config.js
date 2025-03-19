0ec731403585c8b44e3592942baf447b537212d0
Margaux REVELLE MONTEIRO <margaux.revelle@esme.fr>
2025-03-19 12:40:30 +0100

    commit

---
+++ Back/firebase-config.js
@@ -0,0 +1,18 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDBd9qGSmQITJMPjQXqpjba3e1XGU3lZvg",
+   authDomain: "a2msi-js.firebaseapp.com",
+   databaseURL: "https://a2msi-js-default-rtdb.europe-west1.firebasedatabase.app",
+   projectId: "a2msi-js",
+   storageBucket: "a2msi-js.firebasestorage.app",
+   messagingSenderId: "363843091318",
+   appId: "1:363843091318:web:392b7a74a9757c0883d857"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
